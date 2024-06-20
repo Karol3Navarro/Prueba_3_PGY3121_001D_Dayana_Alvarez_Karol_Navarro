3a80aa18a75d8ccfae122e32bb3ecdc312951812
day-alvarez <135995100+day-alvarez@users.noreply.github.com>
2024-06-19 22:49:53 -0400

    se agrego las demas funciones

--- productos/urls.py
+++ productos/urls.py
@@ -6,4 +6,14 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('nuevos_prod',views.nuevos_prod, name='nuevos_prod'),
+    path('adopta',views.adopta, name='adopta'),
+    path('registro',views.registro, name='registro'),
+    path('nosotros',views.nosotros, name='nosotros'),
+    path('bandana',views.bandana, name='bandana'),
+    path('correa',views.correa, name='correa'),
+    path('identificacion',views.identificacion, name='identificacion'),
+    path('contacto',views.contacto, name='contacto'),
+    path('carrito',views.carrito, name='carrito'),
+    path('socio',views.socio, name='socio'),
+
 ]
--- productos/views.py
+++ productos/views.py
@@ -10,3 +10,31 @@ def nuevos_prod(request):
     producto = Producto.objects.all()
     context={'producto':producto}
     return render(request,'productos/nuevos_prod.html', context)
+def adopta(request):
+    context={}
+    return render (request, 'productos/adopta.html', context)
+def registro(request):
+    context={}
+    return render (request, 'productos/registro.html', context)
+def nosotros(request):
+    context={}
+    return render (request, 'productos/nosotros.html', context)
+def bandana(request):
+    context={}
+    return render (request, 'productos/bandana.html', context)
+def correa(request):
+    context={}
+    return render (request, 'productos/correa.html', context)
+def identificacion(request):
+    context={}
+    return render (request, 'productos/identificacion.html', context)
+def contacto(request):
+    context={}
+    return render (request, 'productos/contacto.html', context)
+def carrito(request):
+    context={}
+    return render (request, 'productos/carrito.html', context)
+def socio(request):
+    context={}
+    return render (request, 'productos/socio.html', context)
+
