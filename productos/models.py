e16d2cc8f8251df771a647c3a7dd443ba5159691
Karol3Navarro <karo.navarro@duocuc.cl>
2024-06-19 21:32:53 -0400

    Creacion de models

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. popsipatitas
+class Categoria(models.Model):
+    id_categoria = models.AutoField(db_column='idCategoria', primary_key=True)
+    catagoria = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.catagoria)
+
+class Producto(models.Model):
+    id_producto = models.AutoField(db_column='idProducto', primary_key=True)
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField(null=False)
+    imagen = models.ImageField(upload_to='producto', null=False)
+    cantidad = models.IntegerField(null=False)
+    id_categoria = models.ForeignKey('Categoria',on_delete=models.CASCADE, db_column='idCategoria')
+    activo = models.IntegerField()
+
+    def __str__(self):
+        return str(self.nombre)+" "+str(self.precio)+" "+str(self.imagen)+" "+str(self.cantidad)+" "+str(self.id_categoria)+" "+str(self.activo)
