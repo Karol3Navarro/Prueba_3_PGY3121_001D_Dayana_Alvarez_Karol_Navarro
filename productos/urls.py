ef16bd8fd177db5103ed3ed4946ec36bc188ed08
Karol3Navarro <karo.navarro@duocuc.cl>
2024-06-21 18:48:31 -0400

    CRUD

--- productos/urls.py
+++ productos/urls.py
@@ -15,5 +15,8 @@ urlpatterns = [
     path('contacto',views.contacto, name='contacto'),
     path('carrito',views.carrito, name='carrito'),
     path('socio',views.socio, name='socio'),
-
+    path('productosAdd',views.productosAdd, name='productosAdd'),
+    path('productos_del/<str:pk>',views.productos_del, name='productos_del'),
+    path('productos_findEdit/<str:pk>', views.productos_findEdit, name='productos_findEdit'),
+    path('productosUpdate', views.productosUpdate, name='productosUpdate'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Producto
+from .models import Producto, Categoria
 
 # Create your views here.
 def index(request):
@@ -37,4 +37,89 @@ def carrito(request):
 def socio(request):
     context={}
     return render (request, 'productos/socio.html', context)
+
+def productosAdd(request):
+    if request.method != "POST":
+        categorias=Categoria.objects.all()
+        context={'categorias':categorias}
+        return render(request, 'productos/productos_add.html', context)
+    else:
+        # id_producto=request.POST["id_producto"]
+        nombre=request.POST["nombre"]
+        precio=request.POST["precio"]
+        imagen=request.POST["imagen"]
+        cantidad=request.POST["cantidad"]
+        categoria=request.POST["categoria"]
+        activo="1"
+
+        objCategoria=Categoria.objects.get(id_categoria = categoria)
+        obj=Producto.objects.create(  #id_producto=id_producto,
+                                    nombre=nombre,
+                                    precio=precio,
+                                    imagen=imagen,
+                                    cantidad=cantidad,
+                                    id_categoria=objCategoria,
+                                    activo=1  )
+        obj.save()
+        context={'mensaje':"Ok, producto grabado..."}
+        return render(request, 'productos/productos_add.html', context)
+
+def productos_del(request,pk):
+    context={}
+    try:
+        producto=Producto.objects.get(id_producto=pk)
+
+        producto.delete()
+        mensaje="Bien, datos eliminados..."
+        productos=Producto.objects.all()
+        context={'productos':productos, 'mensaje':mensaje}
+        return render(request, 'productos/nuevos_prod.html', context)
+    except:
+        mensaje="Error, producto no existe..."
+        productos=Producto.objects.all()
+        context={'productos':productos, 'mensaje':mensaje}
+        return render(request, 'productos/nuevos_prod.html',context)
+
+def productos_findEdit(request, pk):
+    if pk != "":
+        producto=Producto.objects.get(id_producto=pk)
+        categorias=Categoria.objects.all()
+
+        # print(type(producto.id_categoria.categoria))
+
+        context={'producto':producto, 'categorias':categorias}
+        if producto:
+            return render(request, 'productos/productos_edit.html', context)
+        else:
+            context={'mensaje':'Error, producto no existe...'}
+            return render(request, 'productos/nuevos_prod.html', context)
+
+def productosUpdate(request):
+    if request.method == "POST":
+        id_producto=request.POST["id_producto"]
+        nombre=request.POST["nombre"]
+        precio=request.POST["precio"]
+        imagen=request.POST["imagen"]
+        cantidad=request.POST["cantidad"]
+        categoria=request.POST["categoria"]
+        activo="1"
+
+        objCategoria = Categoria.objects.get(id_categoria = categoria)
+        producto = Producto()
+        producto.id_producto = id_producto
+        producto.nombre = nombre
+        producto.precio = precio
+        producto.imagen = imagen
+        producto.cantidad = cantidad
+        producto.id_categoria = objCategoria
+        producto.activo = 1
+        producto.save()
+
+        categorias= Categoria.objects.all()
+        context={'mensaje': "Ok, datos actualizados...", 'categorias':categorias, 'producto':producto}
+        return render(request, 'productos/productos_edit.html',context)
+    else:
+        productos = Producto.objects.all()
+        context={'productos':productos}
+        return render(request, 'productos/nuevos_prod.html', context)
 
